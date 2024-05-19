fc535dbf087827e8318a72045f501ad155b755d9
anna <anna9ne@gmail.com>
2024-05-19 19:31:50 +0300

    Create Not Found Page

---
+++ backend/src/views/NotFound.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="flex justify-center items-center min-h-full w-full min-w-full">
+        <div class="text-indigo-600 text-7xl px-8 font-bold border-r pb-8">404</div>
+        <div class="px-8">
+            <h1 class="text-5xl font-bold mb-2">Page not found</h1>
+            <p class="text-gray-600 mb-8">Sorry, we couldn’t find the page you’re looking for.</p>
+            <div>
+                <router-link :to="{name: 'app.dashboard'}" class="rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Go back home</router-link>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+
+
+<style scoped>
+
+</style>
