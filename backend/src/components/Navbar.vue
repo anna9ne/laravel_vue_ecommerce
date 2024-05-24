1c15c3c349055614130b33f28451aad98ed52ca9
anna <anna9ne@gmail.com>
2024-05-24 23:12:25 +0300

    Implement Login/Logout Full Flow

--- backend/src/components/Navbar.vue
+++ backend/src/components/Navbar.vue
@@ -46,6 +46,7 @@
                             </MenuItem>
                             <MenuItem v-slot="{ active }">
                                 <button
+                                    @click="logout"
                                     :class="[
                   active ? 'bg-indigo-500 text-white' : 'text-gray-900',
                   'group flex w-full items-center rounded-md px-2 py-2 text-sm',
@@ -71,8 +72,17 @@
 import { Bars3Icon, ArrowRightStartOnRectangleIcon, UserIcon } from '@heroicons/vue/24/outline'
 import { Menu, MenuButton, MenuItems, MenuItem } from '@headlessui/vue'
 import { ChevronDownIcon } from '@heroicons/vue/24/solid'
+import store from "../store/index.js";
+import router from "../router/index.js";
 
 const emit = defineEmits(['toggle-sidebar'])
+
+function logout() {
+    store.dispatch('logout')
+        .then(() => {
+            router.push({name: 'login'})
+        })
+}
 
 </script>
 
--- backend/src/views/Login.vue
+++ backend/src/views/Login.vue
@@ -1,11 +1,11 @@
 
 <template>
     <guest-layout title="Sign in to your account">
-        <form class="space-y-6" action="#" method="POST">
+        <form class="space-y-6" method="POST" @submit.prevent="login">
             <div>
                 <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
                 <div class="mt-2">
-                    <input id="email" name="email" type="email" autocomplete="email" required="" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                    <input id="email" name="email" type="email" autocomplete="email" required v-model="user.email" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
                 </div>
             </div>
 
@@ -17,8 +17,14 @@
                     </div>
                 </div>
                 <div class="mt-2">
-                    <input id="password" name="password" type="password" autocomplete="current-password" required="" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                    <input id="password" name="password" type="password" autocomplete="current-password" required v-model="user.password" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
                 </div>
+            </div>
+
+            <div class="flex items-center">
+                <input id="remember-me" name="remember-me" type="checkbox" v-model="user.remember"
+                       class="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"/>
+                <label for="remember-me" class="ml-2 block text-sm text-gray-900"> Remember me </label>
             </div>
 
             <div>
@@ -28,8 +34,33 @@
     </guest-layout>
 </template>
 
-<script setup lang="ts">
+<script setup>
+import {ref} from 'vue'
 import GuestLayout from "../components/GuestLayout.vue";
+import store from "../store";
+import router from "../router";
+
+let loading = ref(false)
+let errorMsg = ref("")
+
+const user = {
+    email: '',
+    password: '',
+    remember: false
+}
+
+function login() {
+    loading.value = true
+    store.dispatch('login', user)
+        .then(() => {
+            loading.value = false
+            router.push({name: 'app.dashboard'})
+        })
+        .catch(({response}) => {
+            loading.value = false
+            errorMsg.value = response.data.message
+        })
+}
 </script>
 
 
