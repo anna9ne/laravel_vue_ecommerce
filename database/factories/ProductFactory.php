077f050956207f83fcd2df116636629114e076ac
anna <anna9ne@gmail.com>
2024-06-01 12:47:41 +0300

    Implement Product CRUD in Laravel API

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Testing\Fakes\Fake;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->text(30),
+            'image' => fake()->imageUrl(),
+            'description' => fake()->text(2000),
+            'price' => fake()->randomFloat(2, 10, 5000),
+            'created_at' => now(),
+            'updated_at' => now(),
+            'created_by' => 1,
+            'updated_by' => 1,
+        ];
+    }
+}
